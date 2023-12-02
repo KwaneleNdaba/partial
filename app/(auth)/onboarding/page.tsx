dce7494836bb9c4f58df2d2104bcc40da15e17dd
princeWeparam <price@webparam.org>
2023-12-02 22:55:10 +0200

    build fixes

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,18 +1,32 @@
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from '@clerk/nextjs'
+
+interface Props {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  }
+}
+
+
 async function Page() {
   const user = await currentUser();
 
-  const userInfo = {}
+  const userInfo = await fetchUser(user!.id)
 
   const userData = {
-      id : user?.id,
+      id : user!.id,
       objectId: userInfo?._id,
       username : userInfo?.username || user?.username ,
       name : userInfo?.name || user?.firstName || "",
       bio : userInfo?.bio || "",
       image : userInfo?.image || user?.imageUrl
-  }
+  } 
   return (
     <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
       <h1 className="head-text">Onboarding</h1>
